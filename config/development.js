330dbb6072d7d856f8131d6ec1e3bfb0a1c9a32d
AndreyFizer <Fizer.AP@outlook.com>
2015-10-02 23:28:38 +0300

    first_commit

--- config/development.js
+++ config/development.js
@@ -1,3 +1,11 @@
 /**
  * Created by Andrey on 02.10.2015.
  */
+
+'use strict';
+var path = require('path');
+
+process.env.HOST = 'http://localhost:8877';
+process.env.PORT = 8877;
+process.env.DB_HOST = 'localhost';
+process.env.DB_NAME = 'fizerDev';
